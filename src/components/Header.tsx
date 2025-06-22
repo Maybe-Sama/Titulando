ae4efa3af23a04f804102901fe54ccc6af5a29d3
Maybe-Sama <alerubik@gmail.com>
2025-06-22 23:02:05 +0200

    refactor: Unify language to be consistently impersonal and professional

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -41,7 +41,7 @@ const Header = () => {
             </div>
             <div>
               <h1 className="text-xl font-display font-bold text-neutral-dark">Titulando</h1>
-              <p className="text-sm text-primary-500 font-medium">Asesoramiento experto para un TFG/TFM de sobresaliente</p>
+              <p className="text-sm text-primary-500 font-medium">Asesoramiento experto para un TFG/TFM</p>
             </div>
           </Link>
 
